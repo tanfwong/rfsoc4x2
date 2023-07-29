e025803d0d5af6023efb73d45046e87c21778096
Tan F Wong <138139978+tanfwong@users.noreply.github.com>
2023-07-29 01:23:57 -0400

    Create set_ref_clocks.py

---
+++ src/vitis_adc_platform/set_ref_clocks.py
@@ -0,0 +1,18 @@
+# Hacked out the from RFSoC-PYNQ distribution
+# Tan F. Wong 7/20/2023
+
+from xrfclk import set_ref_clks
+from xrfclk.gpio import GPIO
+
+# Seems to need these to turn on the LMK chip
+lmk_reset = GPIO(341, 'out')
+lmk_clk_sel0 = GPIO(342, 'out')
+lmk_clk_sel1 = GPIO(346, 'out')
+
+lmk_reset.write(1)
+lmk_reset.write(0)
+lmk_clk_sel0.write(0)
+lmk_clk_sel1.write(0)
+
+# Set LMK and LMX freq
+set_ref_clks(lmk_freq=245.76, lmx_freq=491.52)
