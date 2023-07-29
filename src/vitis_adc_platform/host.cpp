7430f239e2204024fa4098a0ce0d9c91a5fd801e
Tan F Wong <138139978+tanfwong@users.noreply.github.com>
2023-07-28 21:06:13 -0400

    Create host.cpp

---
+++ src/vitis_adc_platform/host.cpp
@@ -0,0 +1,106 @@
+#include "xcl2.hpp"
+#include "ap_int.h"
+#include <algorithm>
+#include <vector>
+#include <stdio.h>
+#define DATA_SIZE 8192
+#define DATA_WIDTH
+
+typedef ap_uint<DATA_WIDTH> data_t;
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cout << "Usage: " << argv[0] << " <XCLBIN File>" << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    std::string binaryFile = argv[1];
+    size_t vector_size_bytes = sizeof(data_t) * DATA_SIZE;
+    cl_int err;
+    cl::Context context;
+    cl::Kernel krnl;
+    cl::CommandQueue q;
+    // Allocate Memory in Host Memory
+    // When creating a buffer with user pointer (CL_MEM_USE_HOST_PTR), under the
+    // hood user ptr
+    // is used if it is properly aligned. when not aligned, runtime had no choice
+    // but to create
+    // its own host side buffer. So it is recommended to use this allocator if
+    // user wish to
+    // create buffer using CL_MEM_USE_HOST_PTR to align user buffer to page
+    // boundary. It will
+    // ensure that user buffer is used when user create Buffer/Mem object with
+    // CL_MEM_USE_HOST_PTR
+    std::vector<data_t, aligned_allocator<data_t> > source_hw_data(DATA_SIZE);
+    for (int i = 0; i < DATA_SIZE; i++) {
+        source_hw_data[i] = 99;
+    }
+
+    // OPENCL HOST CODE AREA START
+    // get_xil_devices() is a utility API which will find the xilinx
+    // platforms and will return list of devices connected to Xilinx platform
+    auto devices = xcl::get_xil_devices();
+    // read_binary_file() is a utility API which will load the binaryFile
+    // and will return the pointer to file buffer.
+    auto fileBuf = xcl::read_binary_file(binaryFile);
+    cl::Program::Binaries bins{{fileBuf.data(), fileBuf.size()}};
+    bool valid_device = false;
+    for (unsigned int i = 0; i < devices.size(); i++) {
+        auto device = devices[i];
+        // Creating Context and Command Queue for selected Device
+        OCL_CHECK(err, context = cl::Context(device, nullptr, nullptr, nullptr, &err));
+        OCL_CHECK(err, q = cl::CommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &err));
+        std::cout << "Trying to program device[" << i << "]: " << device.getInfo<CL_DEVICE_NAME>() << std::endl;
+        cl::Program program(context, {device}, bins, nullptr, &err);
+        if (err != CL_SUCCESS) {
+            std::cout << "Failed to program device[" << i << "] with xclbin file!\n";
+        } else {
+            std::cout << "Device[" << i << "]: program successful!\n";
+            OCL_CHECK(err, krnl = cl::Kernel(program, "dummy_kernel", &err));
+            valid_device = true;
+            break; // we break because we found a valid device
+        }
+    }
+    if (!valid_device) {
+        std::cout << "Failed to program any device found, exit!\n";
+        exit(EXIT_FAILURE);
+    }
+
+    // Allocate Buffer in Global Memory
+    // Buffers are allocated using CL_MEM_USE_HOST_PTR for efficient memory and
+    // Device-to-host communication
+    OCL_CHECK(err, cl::Buffer buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, vector_size_bytes,
+                                            source_hw_data.data(), &err));
+
+    unsigned int size = DATA_SIZE;
+    OCL_CHECK(err, err = krnl.setArg(0, buffer));
+    OCL_CHECK(err, err = krnl.setArg(2, size));
+
+    // Launch the Kernel
+    // For HLS kernels global and local size is always (1,1,1). So, it is
+    // recommended
+    // to always use enqueueTask() for invoking HLS kernel
+    OCL_CHECK(err, err = q.enqueueTask(krnl));
+
+    std::cout << "Read data from device\n";
+    // Copy Result from Device Global Memory to Host Local Memory
+    OCL_CHECK(err, err = q.enqueueMigrateMemObjects({buffer}, CL_MIGRATE_MEM_OBJECT_HOST));
+    q.finish();
+    // OPENCL HOST CODE AREA END
+
+    std::cout << "Write data to wave.txt\n";
+    FILE *fp;
+    fp = fopen("wave.txt","w");
+
+    int shortsize = sizeof(data_t)/2;
+    for (int i = 0; i < DATA_SIZE; i++) {
+        data_t sample = source_hw_data[i];
+        fprintf(fp,"%d\n", (short) (sample & 0xFFFF));
+        for (int j = 1;j < shortsize; j++) {
+                sample >>= 16;
+                fprintf(fp,"%d\n", (short) (sample & 0xFFFF));
+        }
+    }
+    fclose(fp);
+
+}                                                                                                   103,9         Bo
