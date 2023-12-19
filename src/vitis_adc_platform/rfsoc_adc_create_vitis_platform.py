1ac8184972e5badf24a529bef0fa572f6b52bafa
Tan F Wong <138139978+tanfwong@users.noreply.github.com>
2023-12-19 13:54:49 -0500

    Update rfsoc_adc_create_vitis_platform.py

--- src/vitis_adc_platform/rfsoc_adc_create_vitis_platform.py
+++ src/vitis_adc_platform/rfsoc_adc_create_vitis_platform.py
@@ -1,3 +1,5 @@
+# Tan F. Wong 12/19/2023
+
 import vitis
 import os
 
@@ -6,6 +8,16 @@ client = vitis.create_client()
 
 # Set workspace 
 workspace = os.path.expanduser('~')+'/workspace'
+os.chdir(workspace)
 client.set_workspace(workspace)
 
 # Create platform
+platform_name = 'rfsoc_adc_vitis_platform'
+platform_description = 'A Vitis extensible platform with 1 ADC for the RFSoC4x2 board'
+platform_hw = 'rfsoc_adc_hardware/rfsoc_adc_hardware.xsa'
+xrt_domain_name = 'linux_xrt'
+
+platform = client.create_platform_component(name=platform_name, desc=platform_description, \
+                                            hw=platform_hw, emulation_xsa_path=platform_hw, \
+                                            cpu="psu_cortexa53", no_boot_bsp=True, \
+                                            os="linux", domain_name=xrt_domain_nam)
