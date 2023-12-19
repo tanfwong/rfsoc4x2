d86c03608e975838219e4e6430e27dfa0594f12c
Tan F Wong <138139978+tanfwong@users.noreply.github.com>
2023-12-19 18:01:36 -0500

    Update rfsoc_adc_create_vitis_platform.py

--- src/vitis_adc_platform/rfsoc_adc_create_vitis_platform.py
+++ src/vitis_adc_platform/rfsoc_adc_create_vitis_platform.py
@@ -15,9 +15,14 @@ client.set_workspace(workspace)
 platform_name = 'rfsoc_adc_vitis_platform'
 platform_description = 'A Vitis extensible platform with 1 ADC for the RFSoC4x2 board'
 platform_hw = 'rfsoc_adc_hardware/rfsoc_adc_hardware.xsa'
-xrt_domain_name = 'linux_xrt'
+platform_domain = 'linux_xrt'
+platform_fsbl = workspace+'/rfsoc-linux/images/linux/zynqmp_fsbl.elf'
+platform_pmu = workspace+'/rfsoc-linux/images/linux/pmufw.elf'
 
 platform = client.create_platform_component(name=platform_name, desc=platform_description, \
                                             hw=platform_hw, emulation_xsa_path=platform_hw, \
-                                            cpu="psu_cortexa53", no_boot_bsp=True, \
-                                            os="linux", domain_name=xrt_domain_nam)
+                                            cpu='psu_cortexa53', no_boot_bsp=True, \
+                                            fsbl_path=platform_fsbl, pmufw_Elf=platform_pmu,\
+                                            os='linux', domain_name=platform_domain)
+
+
